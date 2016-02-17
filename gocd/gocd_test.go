3869c3355b71733d69bb48c52872c0b686b14b8a
Francisco Rodriguez <franciso.rodriguez@cfpb.gov>
2016-02-17 17:34:37 -0500

    added functions to GET and POST to a gocd server

--- gocd/gocd_test.go
+++ gocd/gocd_test.go
@@ -4,10 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/davecgh/go-spew/spew"
+	//"github.com/davecgh/go-spew/spew"
 	"io"
-	"io/ioutil"
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -15,32 +15,39 @@ var gocd_pipeline_config []byte
 
 func init() {
 	buf := bytes.NewBuffer(nil)
-	f, _ := os.Open("gocd_pipeline_config.json")
+	f, _ := os.Open("./test.json")
 	io.Copy(buf, f)
 	f.Close()
 	gocd_pipeline_config = buf.Bytes()
 }
 
 func TestMarshalJSONHAL(t *testing.T) {
-	file, e := ioutil.ReadFile("./gocd_pipeline_config.json")
-	if e != nil {
-		t.Error("file can't be loaded")
-	}
-
-	var prettyJSON bytes.Buffer
-	e = json.Indent(&prettyJSON, file, "", "\t")
-	if e != nil {
-		t.Error("JSON parse error")
-	}
-	fmt.Println("gocd_pipeline_config:", string(prettyJSON.Bytes()))
-
-	var pipeline Pipeline
-	e = json.Unmarshal(file, &pipeline)
-	if e != nil {
-		t.Error("Unmarshaling error")
-	}
-	spew.Dump(pipeline)
+	var pipeline Pipeline = ReadPipelineJSONFromFile("./test.json")
+	//spew.Dump(pipeline)
 
 	fmt.Printf("Pipeline Name: %+v\n", pipeline.Name)
 	fmt.Printf("Pipeline Git URL: %v:%v\n", pipeline.Materials[0].Attributes.URL, pipeline.Materials[0].Attributes.Branch)
+}
+
+// TestUnmarshalFidelityLoss checks that data can be reserielized without fidelity loss
+func TestUnmarshalFidelityLoss(t *testing.T) {
+	data := UnmarshalPipeline(gocd_pipeline_config)
+	gocd_pipeline_config2, _ := json.Marshal(data)
+	data2 := UnmarshalPipeline(gocd_pipeline_config2)
+	if !reflect.DeepEqual(data, data2) {
+		t.Error("not the same")
+	}
+	fmt.Println("TestUnmarshalFidelityLoss")
+}
+
+func TestGocdPOST(t *testing.T) {
+	pipeline := ReadPipelineJSONFromFile("./test.json")
+	pipelineConfig := PipelineConfig{"Dev", pipeline}
+	pipelineConfigPOST("http://localhost:8153", pipelineConfig)
+	fmt.Println("TestGocdPOST")
+}
+
+func TestGocdGET(t *testing.T) {
+	pipelineGET("http://localhost:8153", "test")
+	fmt.Println("TestGocdGET")
 }
