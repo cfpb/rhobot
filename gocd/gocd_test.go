d299171dff3c99e9c1a5f979dd5df727a59a0ac9
Francisco Rodriguez <franciso.rodriguez@cfpb.gov>
2016-02-22 18:05:11 -0500

    gocd pkg
    make Pull/Push/Clone & ReadPipelineJSONFromFile public. Load GoCD user &
    pass from env variables

--- gocd/gocd_test.go
+++ gocd/gocd_test.go
@@ -23,9 +23,9 @@ func init() {
 
 func TestMarshalJSONHAL(t *testing.T) {
 	fmt.Println("TestMarshalJSONHAL")
-	pipeline, err := ReadPipelineJSONFromFile("./test.json")
-    if err != nil {
-        t.Error(err)
+	pipeline, err := readPipelineJSONFromFile("./test.json")
+	if err != nil {
+		t.Error(err)
 	}
 	//spew.Dump(pipeline)
 	fmt.Printf("Pipeline Name: %+v\n", pipeline.Name)
@@ -34,16 +34,16 @@ func TestMarshalJSONHAL(t *testing.T) {
 
 // TestUnmarshalFidelityLoss checks that data can be reserielized without fidelity loss
 func TestUnmarshalFidelityLoss(t *testing.T) {
-    fmt.Println("TestUnmarshalFidelityLoss")
-	data, err1 := UnmarshalPipeline(gocd_pipeline_config)
+	fmt.Println("TestUnmarshalFidelityLoss")
+	data, err1 := unmarshalPipeline(gocd_pipeline_config)
 	if err1 != nil {
-        t.Error(err1)
+		t.Error(err1)
 	}
 
 	gocd_pipeline_config2, _ := json.Marshal(data)
-	data2, err2 := UnmarshalPipeline(gocd_pipeline_config2)
+	data2, err2 := unmarshalPipeline(gocd_pipeline_config2)
 	if err2 != nil {
-        t.Error(err2)
+		t.Error(err2)
 	}
 
 	if !reflect.DeepEqual(data, data2) {
@@ -53,11 +53,11 @@ func TestUnmarshalFidelityLoss(t *testing.T) {
 
 func TestGocdPOST(t *testing.T) {
 	fmt.Println("TestGocdPOST")
-	pipeline, _ := ReadPipelineJSONFromFile("./test.json")
+	pipeline, _ := readPipelineJSONFromFile("./test.json")
 	pipelineConfig := PipelineConfig{"Dev", pipeline}
 	_, err := pipelineConfigPOST("http://localhost:8153", pipelineConfig)
 	if err != nil {
-        t.Error(err)
+		t.Error(err)
 	}
 }
 
@@ -65,6 +65,6 @@ func TestGocdGET(t *testing.T) {
 	fmt.Println("TestGocdGET")
 	_, err := pipelineGET("http://localhost:8153", "test")
 	if err != nil {
-        t.Error(err)
+		t.Error(err)
 	}
 }
