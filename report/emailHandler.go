faf59d5b2d5653d294ad9d4d3f73e0678dd9c4ce
Francisco Rodriguez <franciso.rodriguez@cfpb.gov>
2016-03-24 11:56:18 -0400

    using gomail

--- report/emailHandler.go
+++ report/emailHandler.go
@@ -1,57 +1,55 @@
 package report
 
 import (
-	"bufio"
 	"io"
-	"net/smtp"
+	"io/ioutil"
+
+	"gopkg.in/gomail.v2"
 )
 
 // EmailHandler initilization should contain any variables used for report
 type EmailHandler struct {
-	StmpHost  string
-	Sender    string
-	Recipient string
-	Html bool
+	SMTPHost   string
+	SMTPPort   int
+	Sender     string
+	Recipients []string
+	Subject    string
+	HTML       bool
 }
 
 // HandleReport consumes ReportReader output, writes to file
 func (eh EmailHandler) HandleReport(reader io.Reader) error {
 
-	client, err := smtp.Dial(eh.StmpHost)
+	msg := gomail.NewMessage()
+
+	msg.SetHeaders(map[string][]string{
+		"From":    {eh.Sender},
+		"To":      eh.Recipients,
+		"Subject": {eh.Subject},
+	})
+
+	reportBytes, err := ioutil.ReadAll(reader)
 	if err != nil {
+		// TODO: print Reading error to logger
+		return err
+	}
+	reportString := string(reportBytes)
+
+	var bodyType string
+	if eh.HTML {
+		bodyType = "text/html"
+	} else {
+		bodyType = "text/plain"
+	}
+
+	msg.SetBody(bodyType, reportString)
+
+	//dialer := gomail.NewDialer(eh.SMTPHost, eh.SMTPPort, "", "")
+	dialer := gomail.Dialer{Host: eh.SMTPHost, Port: eh.SMTPPort}
+	if err := dialer.DialAndSend(msg); err != nil {
 		// TODO: print Dial error to logger
 		return err
 	}
-	defer client.Close()
 
-	client.Mail(eh.Sender)
-	client.Rcpt(eh.Recipient)
-	wc, err := client.Data()
-	if err != nil {
-		// TODO: print Data Writer error to logger
-		return err
-	}
-	defer wc.Close()
-
-	writer := bufio.NewWriter(wc)
-	scanner := bufio.NewScanner(reader)
-
-	if(eh.Html){
-	writer.WriteString("Content-Type:text/html\r\n")
-	}
-
-	for scanner.Scan() {
-		_, err := writer.WriteString(scanner.Text() + "\n")
-		if err != nil {
-			// TODO: print Writing error to logger
-			return err
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		// TODO: print Scanning error to logger
-		return err
-	}
-
-	writer.Flush()
 	return err
 }
