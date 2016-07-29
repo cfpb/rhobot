295bffbfb077873a31d58103f4e76dddcf149b54
Francisco Rodriguez <franciso.rodriguez@cfpb.gov>
2016-07-29 12:52:10 -0400

    PGHandler to execute pg query

--- rhobot_test.go
+++ rhobot_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/cfpb/rhobot/config"
@@ -15,6 +16,31 @@ var conf *config.Config
 func init() {
 	conf = config.NewConfig()
 	conf.SetLogLevel("info")
+}
+
+func TestPostgresHealthCheckReporting(t *testing.T) {
+	cxn := database.GetPGConnection(conf.DBURI())
+	healthChecks, _ := healthcheck.ReadHealthCheckYAMLFromFile("healthcheck/healthchecksAll.yml")
+	results, _ := healthChecks.PreformHealthChecks(cxn)
+	var elements []report.Element
+	for _, val := range results {
+		elements = append(elements, val)
+	}
+	metadata := map[string]interface{}{
+		"name":      healthChecks.Name,
+		"schema":    "public",
+		"table":     "healthchecks",
+		"timestamp": time.Now().Format(time.ANSIC),
+	}
+	rs := report.Set{Elements: elements, Metadata: metadata}
+
+	prr := report.NewPongo2ReportRunnerFromString(healthcheck.TemplateHealthcheckPostgres)
+	pgr := report.PGHandler{Cxn: cxn}
+	reader, err := prr.ReportReader(rs)
+	err = pgr.HandleReport(reader)
+	if err != nil {
+		t.Fatalf("error writing report to PG database\n%s", err)
+	}
 }
 
 func TestLogLevelingFiltering(t *testing.T) {
